d852914296dde4e67e65f5463d123741cef8ef27
DefconDios <147849025+DefconDios@users.noreply.github.com>
2023-10-14 19:43:57 +0200

    search bar component (#1)
    * first commit
    
    * search bar component

---
+++ code/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SearchView from './views/search-view/SearchView';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchView/>
+    </div>
+  );
+}
+
+export default App;
