2f9c4a8ed4b92f7dc2ca73990d9ec6d5ed76d348
DefconDios <lbednare@emeal.nttdata.com>
2023-10-14 19:42:30 +0200

    search bar component

---
+++ code/src/views/search-view/SearchView.tsx
@@ -0,0 +1,12 @@
+import SearchBarComponent from "../../components/search-bar/SearchBarComponent";
+import './Search.scss';
+
+const SearchView = () => {
+  return (
+    <div className="search-view">
+      <SearchBarComponent />
+    </div>
+  );
+};
+
+export default SearchView;
