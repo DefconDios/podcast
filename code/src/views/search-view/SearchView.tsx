55342c5552ca037b16876f6be6137c86439a765f
DefconDios <147849025+DefconDios@users.noreply.github.com>
2023-10-16 09:28:06 +0200

    play bar (#2)

--- code/src/views/search-view/SearchView.tsx
+++ code/src/views/search-view/SearchView.tsx
@@ -1,10 +1,12 @@
+import PlayBarComponent from "../../components/play-bar/PlayBarComponent";
 import SearchBarComponent from "../../components/search-bar/SearchBarComponent";
-import './Search.scss';
+import "./Search.scss";
 
 const SearchView = () => {
   return (
     <div className="search-view">
       <SearchBarComponent />
+      <PlayBarComponent />
     </div>
   );
 };
