24d4e4856c5870438e7cd8240b3761aa0a3f00e0
DefconDios <lbednare@emeal.nttdata.com>
2023-10-17 10:51:40 +0200

    service & appContext

--- code/src/components/table/TableComponent.tsx
+++ code/src/components/table/TableComponent.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useState } from "react";
 import "./Table.scss";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -8,26 +8,32 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import ThumbnailComponent from "../thumbnail/ThumbnailComponent";
-
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+import { useAppContext } from "../../AppContext";
+import PauseRounded from "@mui/icons-material/PauseRounded";
+import PlayArrowRounded from "@mui/icons-material/PlayArrowRounded";
+import IconButton from "@mui/material/IconButton";
+import { useTheme } from "@mui/material/styles";
+import { MillisecondsToMinutesSeconds } from "../../utils/Functions";
+import moment from "moment";
+import { Result } from "../../core/models/itunes-data.model";
+import AccessTimeRoundedIcon from "@mui/icons-material/AccessTimeRounded";
 
 const TableComponent = () => {
+  const theme = useTheme();
+  const { searchResults, setSelectedSong } = useAppContext();
+  const [playingRow, setPlayingRow] = useState<number | null>(null); // Pista actualmente en reproducción
+  const mainIconColor = theme.palette.mode === "dark" ? "#fff" : "#000";
+
+  const handleTogglePlayback = (row: Result) => {
+    if (row.trackId === playingRow) {
+      setPlayingRow(null); // Detener reproducción si ya se está reproduciendo
+      setSelectedSong(null);
+    } else {
+      setPlayingRow(row.trackId); // Iniciar reproducción
+      setSelectedSong(row);
+    }
+  };
+
   return (
     <div>
       <ThumbnailComponent />
@@ -35,47 +41,62 @@ const TableComponent = () => {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow className="table__row">
+              <TableCell className="table__cell">#</TableCell>
+              <TableCell className="table__cell">Title</TableCell>
+              <TableCell className="table__cell">Topic</TableCell>
+              <TableCell className="table__cell">Released</TableCell>
               <TableCell className="table__cell">
-                Dessert (100g serving)
-              </TableCell>
-              <TableCell className="table__cell" align="right">
-                Calories
-              </TableCell>
-              <TableCell className="table__cell" align="right">
-                Fat&nbsp;(g)
-              </TableCell>
-              <TableCell className="table__cell" align="right">
-                Carbs&nbsp;(g)
-              </TableCell>
-              <TableCell className="table__cell" align="right">
-                Protein&nbsp;(g)
+                <AccessTimeRoundedIcon />
               </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                className="table__row"
-                key={row.name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell className="table__name" component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell className="table__cell" align="right">
-                  {row.calories}
-                </TableCell>
-                <TableCell className="table__cell" align="right">
-                  {row.fat}
-                </TableCell>
-                <TableCell className="table__cell" align="right">
-                  {row.carbs}
-                </TableCell>
-                <TableCell className="table__cell" align="right">
-                  {row.protein}
-                </TableCell>
-              </TableRow>
-            ))}
+            {searchResults &&
+              searchResults.results.map((row) => (
+                <TableRow
+                  className="table__row"
+                  key={row.artistName}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell className="table__name" component="th" scope="row">
+                    <IconButton
+                      className="play-bar__button"
+                      aria-label={playingRow ? "play" : "pause"}
+                      onClick={() => handleTogglePlayback(row)}
+                    >
+                      {row.trackId === playingRow ? (
+                        <PauseRounded
+                          sx={{ fontSize: "3rem" }}
+                          htmlColor={mainIconColor}
+                        />
+                      ) : (
+                        <PlayArrowRounded
+                          sx={{ fontSize: "3rem" }}
+                          htmlColor={mainIconColor}
+                        />
+                      )}
+                    </IconButton>
+                  </TableCell>
+                  <TableCell className="table__adjust table__cell">
+                    <img src={row.artworkUrl60} alt={row.artworkUrl60} />
+                    <div className="table__column">
+                      <span>{row.trackName}</span>
+                      <span>{row.artistName}</span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="table__cell">
+                    {row.collectionName}
+                  </TableCell>
+                  <TableCell className="table__cell table__nowrap">
+                    {moment(row.releaseDate).utc().format("DD-MM-YYYY")}
+                  </TableCell>
+                  <TableCell className="table__cell">
+                    <MillisecondsToMinutesSeconds
+                      milliseconds={row.trackTimeMillis}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
